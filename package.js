fc5de431a10b3ea383c8661a50af8f57272ac6eb
Jimmie Butler <jimmiebtlr@gmail.com>
2015-12-09 10:32:50 -0600

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'poetic:accounts-passwordless-email',
-  version: '0.0.8',
+  version: '0.0.9',
   summary: 'create and login users without requiring a password',
   git: 'https://github.com/poetic/accounts-passwordless-email',
   documentation: 'README.md'
